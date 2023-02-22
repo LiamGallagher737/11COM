7383b3f047545c6a39702df2c047b2b2619b796f
Liam Gallagher <ljpgallagher@xtra.co.nz>
2023-02-22 16:24:39 +1300

    cat arm works

--- Cats/app.js
+++ Cats/app.js
@@ -1,4 +1,27 @@
+const catArm = document.querySelector('#cat-arm');
+
+catArm.style.left = '-600px';
+
 new fullpage('#fullpage', {
+    licenseKey: 'gplv3-license',
     autoScrolling: true,
     navigation: true,
+    onLeave: function (section, origin, destination, direction, trigger) {
+        let down = destination === 'down';
+
+        catArm.style.transition = 'left 0.2s';
+        catArm.style.left = '-150px';
+        catArm.style.rotate = down ? '20deg' : '-20deg';
+
+        setTimeout(function () {
+            catArm.style.transition = '0.5s';
+            catArm.style.rotate = down ? '-40deg' : '40deg';
+
+            setTimeout(function () {
+                catArm.style.transition = 'left 0.8s';
+                catArm.style.left = '-600px';
+            }, 500);
+
+        }, 100);
+    }
 });
